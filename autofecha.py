551122a0625a467e3661111383de635171aefb43
sh00t72 <113439207+sh00t72@users.noreply.github.com>
2022-09-13 21:24:07 -0300

    Update autofecha.py

--- autofecha.py
+++ autofecha.py
@@ -6,10 +6,10 @@ import locale
 locale.setlocale(locale.LC_ALL , 'es-ar')
 x = datetime.datetime.now()
 x = x + timedelta(hours = 60)
-par = open("fecha mirador PAR.txt", "w")
+par = open("fecha mirador PAR.txt", "w", encoding='windows-1252')
 print("<ASCII-WIN>\n<pstyle:PLANTILLAS\:fecha par>"+x.strftime("%A")+",", x.day,"de", x.strftime("%B"),"de", x.year, file=par)
 par.close()
 
-impar = open("fecha mirador IMPAR.txt", "w")
+impar = open("fecha mirador IMPAR.txt", "w", encoding='windows-1252')
 print("<ASCII-WIN>\n<pstyle:PLANTILLAS\:fecha impar>"+x.strftime("%A")+",", x.day,"de", x.strftime("%B"),"de", x.year, file=impar)
 impar.close()
